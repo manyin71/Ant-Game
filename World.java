1984f72e17e6a8cfe57a889dd85410cafa516dc3
manyin71 <manyin71@hotmail.com>
2014-03-29 20:16:02 +0000

    Update World.java

--- World.java
+++ World.java
@@ -1,3 +1,4 @@
+package world;
 
 public class World {
 	
@@ -7,10 +8,20 @@ public class World {
 		this.cells = cells;
 	}
 	
+	/**
+	 * Returns a cell for an ant to interact with based on their location
+	 * @param x x coordinate on the world
+	 * @param y y coordinate on the world
+	 * @return The cell at coordinates x y
+	 */
 	Cell getCellAt(int x, int y){
 		return cells[x][y];
 	}
 	
+	/**
+	 * Gets all the cells in the world. Used by GameDisplay for updating.
+	 * @return All of the Cell objects held within the world object.
+	 */
 	Cell[][] getAllCells(){
 		return cells;
 	}
